c81bfb5e39eeaf0c6e4eb00a5c3ca84ac9e2276f
samsonovdenys <ds126@libero.it>
2022-02-20 20:16:01 +0100

    PDO and mysqli is working

--- html/index.php
+++ html/index.php
@@ -2,19 +2,34 @@
 //Here is ENRYPOIN for APP
 //phpinfo();
 
-// new mysqli(docker-compose_sql_service_name, DEFAULT_USER="root", docker-compose_MYSQL_ROOT_PASSWORD, DB_NAME);
-$mysqli = new mysqli('mariadb', 'root', 'rootpass', 'budget_db');
 
 // query
 $query = "SELECT spese FROM month_items";
 
-// mysqli function query to make queries. Object returned
-$objResult = $mysqli->query($query);
 
-foreach($objResult as $key => $value){
+// new PDO(docker-compose_sql_service_name, DEFAULT_USER="root", docker-compose_MYSQL_ROOT_PASSWORD);
+$pdo = new PDO('mysql:host=mariadb;dbname=budget_db','root','rootpass');
+// PDO function query to make queries. Object returned
+$objResultPDO = $pdo->query($query);
+foreach($objResultPDO as $key => $value) {
+    print_r('PDO values :');
+    echo "<br>";
     print_r($value);
     echo "<br>";
 }
+
+// new mysqli(docker-compose_sql_service_name, DEFAULT_USER="root", docker-compose_MYSQL_ROOT_PASSWORD, DB_NAME);
+$mysqli = new mysqli('mariadb', 'root', 'rootpass', 'budget_db');
+// mysqli function query to make queries. Object returned
+$objResultMysqli = $mysqli->query($query);
+foreach($objResultMysqli as $key => $value){
+    print_r('mysqli values :');
+    echo "<br>";
+    print_r($value);
+    echo "<br>";
+}
+
+
 
 // Check connection
 if (mysqli_connect_errno()) {
