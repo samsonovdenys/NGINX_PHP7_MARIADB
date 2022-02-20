446312a992eb4957f3380eb2707dc5c3872c72ef
samsonovdenys <ds126@libero.it>
2022-02-20 20:46:39 +0100

    some changes

--- html/index.php
+++ html/index.php
@@ -4,14 +4,14 @@
 
 
 // query
-$query = "SELECT spese FROM month_items";
+$query = "SELECT anno, mese, saldo, stipendio, spese FROM month_items";
 
 
 // new PDO(docker-compose_sql_service_name, DEFAULT_USER="root", docker-compose_MYSQL_ROOT_PASSWORD);
-$pdo = new PDO('mysql:host=mariadb;dbname=budget_db','root','rootpass');
+$pdo = new PDO('mysql:host=mariadb;dbname=budget_db', 'root', 'rootpass');
 // PDO function query to make queries. Object returned
 $objResultPDO = $pdo->query($query);
-foreach($objResultPDO as $key => $value) {
+foreach ($objResultPDO as $key => $value) {
     print_r('PDO values :');
     echo "<br>";
     print_r($value);
@@ -22,7 +22,7 @@ foreach($objResultPDO as $key => $value) {
 $mysqli = new mysqli('mariadb', 'root', 'rootpass', 'budget_db');
 // mysqli function query to make queries. Object returned
 $objResultMysqli = $mysqli->query($query);
-foreach($objResultMysqli as $key => $value){
+foreach ($objResultMysqli as $key => $value) {
     print_r('mysqli values :');
     echo "<br>";
     print_r($value);
@@ -36,4 +36,3 @@ if (mysqli_connect_errno()) {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
     exit();
 }
-
